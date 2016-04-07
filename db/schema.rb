c7736b810b758ea2190ff7b1a1fe4af9e849f168
Abdullah <abd00s@users.noreply.github.com>
2016-04-07 17:53:25 -0400

    Migrate new q counter column to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407165337) do
+ActiveRecord::Schema.define(version: 20160407211746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "questions_count", default: 0
   end
 
   create_table "questions", force: :cascade do |t|
