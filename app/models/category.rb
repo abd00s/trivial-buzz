5e9be9ccf94f98b0367ebfea72f2718435fa1f08
Abdullah <abd00s@users.noreply.github.com>
2016-04-07 17:11:37 -0400

    Define category#appears_in helper method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,13 @@
 class Category < ActiveRecord::Base
   has_many :questions
+
+  # @category.appears_in
+  # Returns instances of Show where @category appeared
+  def appears_in
+    shows = Array.new
+    self.questions.each do |question|
+      shows << question.round.show
+    end
+    shows.uniq
+  end
 end
