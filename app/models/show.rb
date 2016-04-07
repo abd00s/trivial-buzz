aeced6823b66dd2e39c02d3c9662d2ab269fe0f0
Abdullah <abd00s@users.noreply.github.com>
2016-04-07 17:12:40 -0400

    Define show#categories helper method

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,14 @@
 class Show < ActiveRecord::Base
   has_many :rounds
   has_many :questions, through: :rounds
+
+  # @show.categories
+  # Returns all categories appearing in a show
+  def categories
+    categories = Array.new
+    self.questions.each do |question|
+      categories << question.category
+    end
+    categories.uniq
+  end
 end
