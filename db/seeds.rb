8d3bad32cce67521428220a2e586cecbe958f7fb
Abdullah <abd00s@users.noreply.github.com>
2016-04-08 14:19:25 -0400

    Use seeds file to extract from raw json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+json_text = File.read(Rails.root.join('lib', 'seeds', 'questions.json'))
+parsed_json = JSON.parse(json_text)
+count = parsed_json.size
+parsed_json.each do |question|
+
+  s = Show.find_or_create_by(show_number: question["show_number"].to_i) do |show|
+    show.air_date = Date.parse(question["air_date"])
+  end
+
+  r = Round.find_or_create_by(name: question["round"], show: s)
+
+  c = Category.find_or_create_by(name: question["category"].downcase)
+
+  q = Question.new
+  q.body = question["question"]
+  q.response = question["answer"]
+  q.value = if q.value
+    question["value"].gsub(/\D/,'').to_i
+  else
+    0
+  end
+  q.category = c
+  q.round = r
+  q.save
+
+  puts count
+  count -= 1
+
+end
+
+puts "There are now #{Show.count} rows in the shows table"
+puts "There are now #{Round.count} rows in the rounds table"
+puts "There are now #{Category.count} rows in the categories table"
+puts "There are now #{Question.count} rows in the questions table"
+puts "Raw input was #{parsed_json.size} questions"
