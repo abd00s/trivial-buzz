af1340ac4fe590d1f555c603297a57439661ba80
Abdullah <abd00s@users.noreply.github.com>
2016-04-08 17:13:34 -0400

    Create namescaped and versioned api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    scope :v1 do
+      resources :shows, only: :show
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
