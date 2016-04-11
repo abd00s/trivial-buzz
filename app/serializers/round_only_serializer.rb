43dc486becbbee411e173e517f7837e6801ebdef
Abdullah <abd00s@users.noreply.github.com>
2016-04-11 16:32:21 -0400

    Include url to round in question standalone

--- app/serializers/round_only_serializer.rb
+++ app/serializers/round_only_serializer.rb
@@ -1,5 +1,9 @@
 class RoundOnlySerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :round_url
 
   has_one :show, serializer: ShowSimpleSerializer, root: :from_show
+
+  def round_url
+    "#{api_round_url(object.id)}.json"
+  end
 end
