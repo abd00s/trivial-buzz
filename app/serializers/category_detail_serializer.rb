7df871cc674ec976bb3be0d477e8eb3c321005fa
Abdullah <abd00s@users.noreply.github.com>
2016-04-11 17:06:48 -0400

    Display shows where the category appears in the category standalone

--- app/serializers/category_detail_serializer.rb
+++ app/serializers/category_detail_serializer.rb
@@ -1,5 +1,8 @@
 class CategoryDetailSerializer < ActiveModel::Serializer
   attributes :id, :name, :questions_count
 
+  has_many :shows, serializer: ShowSimpleSerializer
+
   has_many :questions, serializer: QuestionSimpleSerializer
+
 end
