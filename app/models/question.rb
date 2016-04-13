0e9de9615a70d39d037405f702cf92dfeb7b6bbb
Abdullah <abd00s@users.noreply.github.com>
2016-04-12 22:07:25 -0400

    Define scopes on Question; random, newer_than and older_than

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,27 @@
 class Question < ActiveRecord::Base
   belongs_to :category, counter_cache: true
   belongs_to :round
+
+  scope :random, -> { order("RANDOM()").first }
+
+  # Only equality, range and subset checking are possible with Hash conditions
+  # (not grater than and less than comparison).
+
+  def self.newer_than(minimum)
+    if minimum.present?
+      newest = Show.order(air_date: :desc).first.air_date
+      includes(round: :show).where({"shows.air_date" => minimum..newest})
+    else
+      all
+    end
+  end
+
+  def self.older_than(maximum)
+    if maximum.present?
+      oldest = Show.order(:air_date).first.air_date
+      includes(round: :show).where({"shows.air_date" => oldest..maximum})
+    else
+      all
+    end
+  end
 end
