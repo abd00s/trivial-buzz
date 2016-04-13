986d3d45fdbbd93aa5c9b63594687d33040f8a22
Abdullah <abd00s@users.noreply.github.com>
2016-04-12 22:08:34 -0400

    Modify questions#random to handle bounded and unbounded in single query

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -5,8 +5,30 @@ class Api::QuestionsController < ApiController
   end
 
   def random
-    record_id = rand(Question.count)
-    @question = Question.find(record_id)
-    respond_with @question, serializer: QuestionOnlySerializer, root: :question
+    # Returns a random question, asked between two dates, if provided.
+    # Example usage:
+    # ../questions/random.json?newer_than=2009-03-02&older_than=2010-02-03
+    # Optional parameters `newer_than` and `older_than` define bounds if present
+    # Query string may include an upper and/or a lower bound or none.
+
+    minimum = Date.parse(params[:newer_than]) if params[:newer_than].present?
+    maximum = Date.parse(params[:older_than]) if params[:older_than].present?
+
+    if minimum.present? && maximum.present? && minimum > maximum
+      render json: {Error: "Invalid range; minimum is greater than maximum."} and return
+    end
+
+    # We make use of chaining scopes defined on Question.
+    @question = Question.newer_than(minimum)
+      .older_than(maximum)
+      .random
+
+    if @question.present?
+      respond_with @question, serializer: QuestionOnlySerializer, root: :question
+    else
+      oldest = Show.order(:air_date).first.air_date
+      newest = Show.order(air_date: :desc).first.air_date
+      render json: {Error: "No questions in selected range; no shows aired in range or bounds are outside of valid range #{oldest} to #{newest}"}
+    end
   end
 end
