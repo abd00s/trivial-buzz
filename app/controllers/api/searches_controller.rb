10301d816cdf0a1207445bc917b070c148f1683b
Abdullah <abd00s@users.noreply.github.com>
2016-04-21 15:40:35 -0400

    Prep results in searches controller; sep Qs and Cs

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,5 +1,15 @@
 class Api::SearchesController < ApiController
   def new
-    render json: {}
+    @query_input = params[:query] if params[:query].present?
+    @results = Search.new(query: @query_input).results
+    @category_results = @results.select{|result| result.class == Category}
+    @question_results = @results.select { |result| result.class == Question }
+    # render json: @results, each_serializer: ResultsSerializer
+    # render json: {type: "#{@results.class}"}
+    # respond_with @results
+  end
+
+  def set_serializer
+
   end
 end
