69d2a615844e2389eb6f25e2b774a02eb1fa7c26
Abdullah <abd00s@users.noreply.github.com>
2016-04-22 15:21:13 -0400

    Add parameter to fetch most popular x categories in categories#index

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,7 +1,17 @@
 class Api::CategoriesController < ApiController
   def index
-    @categories = Category.page(params[:page]).per(10)
-    respond_with @categories, each_serializer: CategoryIndexSerializer, meta: pagination_dict(@categories)
+    @categories =
+      if params[:popular].present?
+        limit = params[:popular].to_i
+        categories_array = Category.most_common(limit).to_a
+        Kaminari.paginate_array(categories_array) # Kaminari workaround; otherwise
+        # call to .per reload "all" categories; and not the limit passed to .most_common
+      else
+        Category.all
+      end.page(params[:page]).per(10)
+
+    respond_with @categories, each_serializer: CategoryIndexSerializer,
+      meta: pagination_dict(@categories)
   end
 
   def show
