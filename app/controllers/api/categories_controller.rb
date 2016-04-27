3807ae85b12396529d6bdf888c0d1031b9e6bbd6
Abdullah <abd00s@users.noreply.github.com>
2016-04-27 17:08:24 -0400

    Rescue bad user input for category count

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -3,6 +3,11 @@ class Api::CategoriesController < ApiController
     @categories =
       if params[:popular].present?
         limit = params[:popular].to_i
+        if limit == 0
+          render json: {Error: "#{params[:popular]} is an invalid count limit. " \
+          + "Proper usage: /categories.json?popular=5 to return 5 most popular categories"} \
+          and return
+        end
         categories_array = Category.most_common(limit).to_a
         Kaminari.paginate_array(categories_array) # Kaminari workaround; otherwise
         # call to .per reloads "all" categories; and not the limit passed to .most_common
