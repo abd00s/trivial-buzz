719d12b33ee0c449ae1c7f94630449f542a58a5d
Abdullah <abd00s@users.noreply.github.com>
2016-05-12 19:47:51 -0400

    Configuartion for swagger engine

---
+++ config/initializers/swagger_engine.rb
@@ -0,0 +1,5 @@
+SwaggerEngine.configure do |config|
+  config.json_files = {
+    v1: "lib/swagger.json"
+  }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
       get 'searches/new'
     end
   end
+  mount SwaggerEngine::Engine, at: "/"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
