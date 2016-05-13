a6e465203ef1785f754a597ab38d8703cc5ef192
Abdullah <abd00s@users.noreply.github.com>
2016-05-13 16:47:11 -0400

    Define swagger meta schema

---
+++ app/models/meta_swagger_model.rb
@@ -0,0 +1,26 @@
+class MetaSwaggerModel
+  include Swagger::Blocks
+
+  swagger_schema :MetaModel do
+    property :current_page do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :next_page do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :prev_page do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :total_pages do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :total_count do
+      key :type, :integer
+      key :format, :int64
+    end
+  end
+end
