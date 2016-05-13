07e4628a1ddaa6145c3de24ab6c34e5e6dd4d978
Abdullah <abd00s@users.noreply.github.com>
2016-05-13 13:15:20 -0400

    Include swagger blocks at the apicontroller level.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,14 +1,16 @@
 class ApiController < ApplicationController
+  include Swagger::Blocks
+
   respond_to :json
 
-def pagination_dict(object)
-  {
-    current_page: object.current_page,
-    next_page: object.next_page,
-    prev_page: object.prev_page,
-    total_pages: object.total_pages,
-    total_count: object.total_count
-  }
-end
+  def pagination_dict(object)
+    {
+      current_page: object.current_page,
+      next_page: object.next_page,
+      prev_page: object.prev_page,
+      total_pages: object.total_pages,
+      total_count: object.total_count
+    }
+  end
 
 end
