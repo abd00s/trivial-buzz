590958c2040b8d8a1b3d7c689bdef42e276e77ea
Abdullah <abd00s@users.noreply.github.com>
2016-10-07 14:45:34 -0400

    Spec /categories/{id}.json endpoint

--- spec/requests/api/v1/category_spec.rb
+++ spec/requests/api/v1/category_spec.rb
@@ -60,7 +60,62 @@ RSpec.describe Category, type: :request do
       it "returns results starting at page 5" do
         get "/api/v1/categories.json?page=5"
 
+        expect(response).to be_success
+
         expect(response_body_as_json["meta"]["current_page"]).to eq(5)
+      end
+    end
+  end
+
+  describe "Endpoint: /categories/{id}.json" do
+    context "with a valid ID" do
+      it "returns a category object with all its relevant data" do
+        get "/api/v1/categories/1.json"
+
+        expect(response).to be_success
+
+        expect(response_body_as_json).to have_key("category")
+
+        expect(response_body_as_json["category"]).to \
+          include("id", "name", "questions_count", "shows", "questions")
+
+        expect(response_body_as_json["category"]["shows"]).to be_an(Array)
+
+        expect(response_body_as_json["category"]["questions"]).to be_an(Array)
+      end
+    end
+
+    context "with a valid ID but no matching record" do
+      it "returns an error object with a 404 status" do
+        invalid_id = Category.count + 1
+        get "/api/v1/categories/#{invalid_id}.json"
+
+        expect(response_body_as_json).to have_key("error")
+
+        expect(response_body_as_json["error"]["status"]).to eq("404")
+      end
+
+      it "returns an error object with a message describing absence of matching record" do
+        invalid_id = Category.count + 1
+        get "/api/v1/categories/#{invalid_id}.json"
+
+        expect(response_body_as_json["error"]["message"]).to match(/no category with ID/)
+      end
+    end
+
+    context "with invalid ID" do
+      it "returns an error object with a 400 status" do
+        get "/api/v1/categories/X.json"
+
+        expect(response_body_as_json).to have_key("error")
+
+        expect(response_body_as_json["error"]["status"]).to eq("404")
+      end
+
+      it "returns an error object with a message describing bad request input" do
+        get "/api/v1/categories/X.json"
+
+        expect(response_body_as_json["error"]["message"]).to match(/no category with ID/)
       end
     end
   end
