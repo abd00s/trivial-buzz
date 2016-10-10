1f97d72a31b9bfdff6796141d8b18639d5705b3c
Abdullah <abd00s@users.noreply.github.com>
2016-10-10 19:44:47 -0400

    Modify seed structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,28 @@
 json_text = File.read(Rails.root.join('lib', 'seeds', 'questions.json'))
 parsed_json = JSON.parse(json_text)
 count = parsed_json.size
-parsed_json.each do |question|
 
-  if Question.find_by(body: question["question"]).present?
+number_of_questions_already_seeded = Question.count - 1
+
+for i in number_of_questions_already_seeded...parsed_json.size
+# parsed_json.each do |question|
+
+  if Question.find_by(body: parsed_json[i]["question"]).present?
     next
   else
-    s = Show.find_or_create_by(show_number: question["show_number"].to_i) do |show|
-      show.air_date = Date.parse(question["air_date"])
+    s = Show.find_or_create_by(show_number: parsed_json[i]["show_number"].to_i) do |show|
+      show.air_date = Date.parse(parsed_json[i]["air_date"])
     end
 
-    r = Round.find_or_create_by(name: question["round"], show: s)
+    r = Round.find_or_create_by(name: parsed_json[i]["round"], show: s)
 
-    c = Category.find_or_create_by(name: question["category"].downcase)
+    c = Category.find_or_create_by(name: parsed_json[i]["category"].downcase)
 
     q = Question.new
-    q.body = question["question"]
-    q.response = question["answer"]
-    q.value = if question["value"]
-      question["value"].gsub(/\D/,'').to_i
+    q.body = parsed_json[i]["question"]
+    q.response = parsed_json[i]["answer"]
+    q.value = if parsed_json[i]["value"]
+      parsed_json[i]["value"].gsub(/\D/,'').to_i
     else
       0
     end
