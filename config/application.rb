a8f43c98ee89dea888e2310c12d1cb73bddadee4
DataNick <DataNick@users.noreply.github.com>
2017-01-02 19:14:19 -0500

    Add rack-cors gem and configuration in application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,12 @@ module Jeopardy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get]
+      end
+    end
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
